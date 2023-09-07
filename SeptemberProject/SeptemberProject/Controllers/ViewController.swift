ba282a1636f1833d1b40495fac9091f13da44510
sergey <beale@ngs.ru>
2023-09-07 14:08:21 +0300

    iteration 1

--- SeptemberProject/SeptemberProject/Controllers/ViewController.swift
+++ SeptemberProject/SeptemberProject/Controllers/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController {
         $0.font = .systemFont(ofSize: 22, weight: .medium)
         $0.textColor = .black
         $0.textAlignment = .center
-        $0.numberOfLines = 0
         $0.translatesAutoresizingMaskIntoConstraints = false
         return $0
     }(UILabel())
@@ -39,21 +38,33 @@ class ViewController: UIViewController {
     
     private let background: UIImageView = {
         $0.image = UIImage(named: "backgroundAuth")
-        $0.contentMode = .scaleAspectFill
+        $0.contentMode = .scaleToFill
         $0.layer.masksToBounds = true
         $0.translatesAutoresizingMaskIntoConstraints = false
+       
+        return $0
+    }(UIImageView())
+    
+    private let backgroundBottom: UIImageView = {
+        $0.image = UIImage(named: "rectangle150")
+        //$0.contentMode = .scaleToFill
+        $0.layer.masksToBounds = true
+        $0.translatesAutoresizingMaskIntoConstraints = false
+       
         return $0
     }(UIImageView())
     
     private let bottomView: UIView = {
         $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.backgroundColor = .white
+        //$0.backgroundColor = .white
+       // $0.layer.zPosition = 5
         return $0
     }(UIView())
     
     private let textFieldsView: UIView = {
         $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.backgroundColor = .yellow
+        $0.backgroundColor = .white
+        //$0.layer.zPosition = 10
         return $0
     }(UIView())
     
@@ -62,21 +73,27 @@ class ViewController: UIViewController {
         $0.tintColor = .white
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.setTitle("Далее", for: .normal)
+        $0.layer.cornerRadius = 32
+        $0.layer.cornerCurve = .continuous
         return $0
     }(UIButton())
     
     lazy var emailTextField: UITextField = {
-        $0.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width-32, height: 30)
+        $0.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width-32, height: 59)
         $0.placeholder = "E-mail"
         $0.borderStyle = .roundedRect
-        
+        $0.layer.cornerRadius = 10
+        $0.layer.cornerCurve = .continuous
         return $0
     }(UITextField())
     
     lazy var passwordTextField: UITextField = {
-        $0.frame = CGRect(x: 0, y: 70, width: self.view.frame.size.width-32, height: 30)
+        $0.frame = CGRect(x: 0, y: 66, width: self.view.frame.size.width-32, height: 59)
         $0.placeholder = "Пароль"
         $0.borderStyle = .roundedRect
+        $0.isSecureTextEntry = true
+        $0.layer.cornerRadius = 10
+        $0.layer.cornerCurve = .continuous
         return $0
     }(UITextField())
     
@@ -84,15 +101,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "backgroundColorAuth")
+        //view.layer.zPosition = 0
+        
+        view.addSubview(background)
         view.addSubview(bottomView)
+        
+        bottomView.addSubview(backgroundBottom)
+        bottomView.addSubview(textFieldsView)
+        
+        textFieldsView.addSubview(enter)
         textFieldsView.addSubview(emailTextField)
         textFieldsView.addSubview(passwordTextField)
-        bottomView.addSubview(textFieldsView)
-        bottomView.addSubview(enter)
+        textFieldsView.addSubview(nextButton)
+
+        
         bottomView.addSubview(resetPassword)
         bottomView.addSubview(registration)
-        bottomView.addSubview(background)
-        bottomView.addSubview(nextButton)
+
+        
+        
         
 
         setupUI()
@@ -103,38 +130,44 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
   
             background.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            background.bottomAnchor.constraint(equalTo: bottomView.topAnchor, constant: 140),
             
             bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
             bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            bottomView.topAnchor.constraint(equalTo: background.bottomAnchor, constant: 0),
+            bottomView.topAnchor.constraint(equalTo: background.bottomAnchor, constant: -140),
             
             resetPassword.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 16),
             resetPassword.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -32),
 
             registration.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -16),
             registration.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -32),
-            
-            nextButton.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -70),
-            nextButton.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 17),
-            nextButton.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -17),
-            nextButton.heightAnchor.constraint(equalToConstant: 30),
+//
+            nextButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 30),
+            nextButton.leadingAnchor.constraint(equalTo: textFieldsView.leadingAnchor, constant: 0),
+            nextButton.trailingAnchor.constraint(equalTo: textFieldsView.trailingAnchor, constant: 0),
+            nextButton.heightAnchor.constraint(equalToConstant: 60),
 
-            textFieldsView.topAnchor.constraint(equalTo: enter.bottomAnchor, constant: 16),
+            //textFieldsView.topAnchor.constraint(equalTo: enter.bottomAnchor, constant: 16),
             textFieldsView.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 16),
             textFieldsView.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -16),
             
             
-            enter.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 15),
+            enter.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 45),
             enter.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 15),
             enter.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -15),
             
+            emailTextField.topAnchor.constraint(equalTo: enter.bottomAnchor, constant: 15),
             emailTextField.leadingAnchor.constraint(equalTo: textFieldsView.leadingAnchor, constant: 0),
             emailTextField.trailingAnchor.constraint(equalTo: textFieldsView.trailingAnchor, constant: 0),
-            
-            
+//
+//
             passwordTextField.leadingAnchor.constraint(equalTo: textFieldsView.leadingAnchor, constant: 16),
             passwordTextField.trailingAnchor.constraint(equalTo: textFieldsView.trailingAnchor, constant: 16),
+//
+            backgroundBottom.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 0),
+            backgroundBottom.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 0),
+            backgroundBottom.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: 0)
             
 
         ])
