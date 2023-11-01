410e71dc0290432385f5dec09de9cab45d65207d
sergey <beale@ngs.ru>
2023-11-01 12:58:49 +0300

    snapkit: customtextField

--- SeptemberProject/SeptemberProject/Extenstions/CustomTextField.swift
+++ SeptemberProject/SeptemberProject/Extenstions/CustomTextField.swift
@@ -25,39 +25,30 @@ final class CustomTextField: UIView {
         static let correctlyTextColor = UIColor(red: 31/255, green: 34/255, blue: 52/255, alpha: 1)
         static let notCorrectlyColor = UIColor(red: 250/255, green: 46/255, blue: 105/255, alpha: 1)
         static let defaultColor = UIColor(red: 237/255, green: 237/255, blue: 240/255, alpha: 1)
-        
-        static let size60: CGFloat = 60
-        static let size15: CGFloat = 15
-        static let size11: CGFloat = 11
     }
     
     private var labelText: String = ""
     private var type: TypeTextField = .none
     private var isCorrectly: Bool = false
-    private var labelTopCon: CGFloat = 0
-    private var textTopCon: CGFloat = 0
-
+    
     //MARK: -- elements
     private let stackView: UIStackView = {
-           let stack = UIStackView(arrangedSubviews: [])
-           stack.axis  = .horizontal
-           stack.alignment = .center
-           stack.translatesAutoresizingMaskIntoConstraints = false
-           return stack
-       }()
+        let stack = UIStackView(arrangedSubviews: [])
+        stack.axis  = .horizontal
+        stack.alignment = .center
+        return stack
+    }()
     
     private let floatingLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 12, weight: .light)
         label.textColor = .black
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private var floatingText: UITextField = {
         let textField = UITextField()
         textField.textColor = .gray
-        textField.translatesAutoresizingMaskIntoConstraints = false
         textField.font = UIFont.systemFont(ofSize: 16, weight: .light)
         textField.addTarget(self, action: #selector(editTextChanged), for: .editingChanged)
         return textField
@@ -82,35 +73,33 @@ final class CustomTextField: UIView {
                 layer.borderColor = Constants.notCorrectlyColor.cgColor
                 floatingText.textColor = Constants.notCorrectlyColor
             }
-            
         } else if size == 0 {
             reset()
         }
     }
     
-    private func updateConst() {
-        NSLayoutConstraint.activate([
-            self.heightAnchor.constraint(equalToConstant: Constants.size60),
-            
-            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: Constants.size15),
-            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: Constants.size11),
-            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -Constants.size15),
-            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -Constants.size11),
-            
-            floatingLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
-            floatingLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
-            floatingLabel.topAnchor.constraint(equalTo: stackView.topAnchor, constant: labelTopCon),
-            
-            floatingText.leadingAnchor.constraint(equalTo: stackView.leadingAnchor, constant: 0),
-            floatingText.trailingAnchor.constraint(equalTo: stackView.trailingAnchor, constant: 0),
-            floatingText.topAnchor.constraint(equalTo: floatingLabel.bottomAnchor, constant: textTopCon)
-        ])
+    private func makeConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.leading.equalTo(self.snp.leading).offset(15)
+            make.top.equalTo(self.snp.top).offset(11)
+            make.trailing.equalTo(self.snp.trailing).offset(-15)
+            make.bottom.equalTo(self.snp.bottom).offset(-11)
+        }
+        
+        floatingLabel.snp.makeConstraints { make in
+            make.leading.equalTo(stackView.snp.leading)
+            make.trailing.equalTo(stackView.snp.trailing)
+            make.top.equalTo(stackView.snp.top).offset(0)
+        }
+       
+        floatingText.snp.makeConstraints { make in
+            make.leading.equalTo(stackView.snp.leading)
+            make.trailing.equalTo(stackView.snp.trailing)
+        }
     }
     
     private func reset() {
         floatingLabel.text = ""
-        labelTopCon = 0
-        textTopCon = 0
         layer.borderColor = Constants.defaultColor.cgColor
     }
     
@@ -120,8 +109,9 @@ final class CustomTextField: UIView {
         self.labelText = placeholder
         floatingText.placeholder = placeholder
         self.type = type
-
+        
         setupUI()
+        makeConstraints()
     }
     
     required init(coder: NSCoder) {
@@ -134,7 +124,6 @@ final class CustomTextField: UIView {
         layer.backgroundColor = UIColor.white.cgColor
         layer.borderColor =  Constants.defaultColor.cgColor
         layer.borderWidth = 1
-        translatesAutoresizingMaskIntoConstraints = false
         layer.borderColor = UIColor(named: "borderColorEditText")!.cgColor
     }
     
@@ -153,7 +142,6 @@ final class CustomTextField: UIView {
         if self.type == .pass {
             floatingText.isSecureTextEntry = true
         }
-        updateConst()
     }
     
     //MARK: -- Проверка различных типов поля
@@ -171,10 +159,10 @@ final class CustomTextField: UIView {
     }
     
     private func isValidEmail() -> Bool {
-          let emailRegEx = "^(?!\\.)([A-Z0-9a-z_%+-]?[\\.]?[A-Z0-9a-z_%+-])+@[A-Za-z0-9-]{1,20}(\\.[A-Za-z0-9]{1,15}){0,10}\\.[A-Za-z]{2,20}$"
-          let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let emailRegEx = "^(?!\\.)([A-Z0-9a-z_%+-]?[\\.]?[A-Z0-9a-z_%+-])+@[A-Za-z0-9-]{1,20}(\\.[A-Za-z0-9]{1,15}){0,10}\\.[A-Za-z]{2,20}$"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: floatingText.text)
-     }
+    }
     
     func getValue() -> String {
         floatingText.text ?? ""
